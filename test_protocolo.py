1f83cbec1c0e998a400c0da49d8cfdd168d311b9
Alejandra Vazquez <alejandra.vazquezcorbella@alumno.ceu.es>
2026-05-25 12:26:36 +0200

    test 2

--- test_protocolo.py
+++ test_protocolo.py
@@ -15,5 +15,14 @@ class TestRespuestaFrase(unittest.TestCase):
         texto = r[len("frase "):]
         self.assertIn(texto, self.frases)
 
+class TestRespuestaTotal(unittest.TestCase):
+    def setUp(self):
+        self.frases = ["Frase uno", "Frase dos", "Frase tres"]
+        self.lineas = 7
+
+    def test_formato_total(self):
+        r = protocolo.generar_respuesta("total", self.frases, self.lineas)
+        self.assertEqual(r, "3 FRASES 7 LINEAS")
+
 if __name__ == "__main__":
     unittest.main()
